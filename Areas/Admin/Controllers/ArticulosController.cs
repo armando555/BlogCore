5a58d7dc2ba8905cc4e11a5d59f36c4226e1ecbe
armando555 <armandosebas4@gmail.com>
2021-06-23 23:56:10 -0500

    Agregar archivos de proyecto.

---
+++ Areas/Admin/Controllers/ArticulosController.cs
@@ -0,0 +1,155 @@
+﻿using BlogCore.AccesoDatos.Data.Repository;
+using BlogCore.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticulosController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public ArticulosController(IContenedorTrabajo contenedorTrabajo, IWebHostEnvironment hostingEnvironment)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ArticuloVM artivm = new ArticuloVM()
+            {
+                Articulo = new Models.Articulo(),
+                ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias()
+            };
+            return View(artivm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ArticuloVM articuloVM)
+        {
+            if (ModelState.IsValid)
+            {
+                string rutaPrincipal = _hostingEnvironment.WebRootPath;
+                var archivos = HttpContext.Request.Form.Files;
+                if (articuloVM.Articulo.Id == 0)
+                {
+                    //nuevo articulo
+                    string nombreArchivo = Guid.NewGuid().ToString();
+                    var subidas = Path.Combine(rutaPrincipal, @"imagenes\articulos");
+                    var extension = Path.GetExtension(archivos[0].FileName);
+                    using (var fileStreams = new FileStream(Path.Combine(subidas,nombreArchivo + extension), FileMode.Create))
+                    {
+                        archivos[0].CopyTo(fileStreams);
+                    }
+                    articuloVM.Articulo.UrlImagen = @"\imagenes\articulos\" + nombreArchivo + extension;
+                    articuloVM.Articulo.FechaCreacion = DateTime.Now.ToString();
+                    _contenedorTrabajo.Articulo.Add(articuloVM.Articulo);
+                    _contenedorTrabajo.Save();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            articuloVM.ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias();
+            return View(articuloVM);
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            ArticuloVM artivm = new ArticuloVM()
+            {
+                Articulo = new Models.Articulo(),
+                ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias()
+            };
+            if (id != null)
+            {
+                artivm.Articulo = _contenedorTrabajo.Articulo.Get(id.GetValueOrDefault());
+            }
+            return View(artivm);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ArticuloVM articuloVM)
+        {
+            if (ModelState.IsValid)
+            {
+                string rutaPrincipal = _hostingEnvironment.WebRootPath;
+                var archivos = HttpContext.Request.Form.Files;
+                var articuloDesdeDb = _contenedorTrabajo.Articulo.Get(articuloVM.Articulo.Id);
+                if (archivos.Count() > 0)
+                {
+                    //Editamos imagen
+                    string nombreArchivo = Guid.NewGuid().ToString();
+                    var subidas = Path.Combine(rutaPrincipal, @"imagenes\articulos");
+                    var extension = Path.GetExtension(archivos[0].FileName);
+                    var nuevaExtension = Path.GetExtension(archivos[0].FileName);
+                    var rutaImagen = Path.Combine(rutaPrincipal, articuloDesdeDb.UrlImagen.TrimStart('\\'));
+
+                    if (System.IO.File.Exists(rutaImagen))
+                    {
+                        System.IO.File.Delete(rutaImagen);
+                    }
+                    //subimos nuevamente el archivo
+                    using (var fileStreams = new FileStream(Path.Combine(subidas, nombreArchivo + nuevaExtension), FileMode.Create))
+                    {
+                        archivos[0].CopyTo(fileStreams);
+                    }
+                    articuloVM.Articulo.UrlImagen = @"\imagenes\articulos\" + nombreArchivo + nuevaExtension;
+                   /* articuloVM.Articulo.FechaCreacion = DateTime.Now.ToString();
+                    _contenedorTrabajo.Articulo.Update(articuloVM.Articulo);
+                    _contenedorTrabajo.Save();
+                    return RedirectToAction(nameof(Index));*/
+                }
+                else
+                {//aquí la imagen ya existe, entonces debe conservar la que ya existe y no se reemplaza
+                    articuloVM.Articulo.UrlImagen = articuloDesdeDb.UrlImagen;
+                }
+                articuloVM.Articulo.FechaCreacion = DateTime.Now.ToString();
+                _contenedorTrabajo.Articulo.Update(articuloVM.Articulo);
+                _contenedorTrabajo.Save();
+                return RedirectToAction(nameof(Index));
+
+            }
+            articuloVM.ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias();
+            return View(articuloVM);
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var articuloDesdeDb = _contenedorTrabajo.Articulo.Get(id);
+            string rutaDirectorioPrincipal = _hostingEnvironment.WebRootPath;
+            var rutaImagen = Path.Combine(rutaDirectorioPrincipal, articuloDesdeDb.UrlImagen.TrimStart('\\'));
+            if (System.IO.File.Exists(rutaImagen))
+            {
+                System.IO.File.Delete(rutaImagen);
+            }
+            if (articuloDesdeDb == null)
+            {
+                return Json(new { success = false, message = "Error borrando artículo"});
+            }
+            _contenedorTrabajo.Articulo.Remove(articuloDesdeDb);
+            _contenedorTrabajo.Save();
+            return Json(new { success = true, message = "Artículo eliminado correctamente"});
+
+        }
+
+        #region llamadas a la API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Articulo.GetAll(includeProperties:"Categoria") });
+        }
+        #endregion
+    }
+}
